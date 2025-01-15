2ab12002e0b8804c9c9d91494a7f609afa3619ff
Mukul Kumar <mukulkumar.codes@gmail.com>
2025-01-15 12:42:34 +0530

    This is the java caomputer code for zigZag product of the two graphs.

--- src/ZigZagProduct.java
+++ src/ZigZagProduct.java
@@ -12,7 +12,7 @@ public class ZigZagProduct {
         degree = input.nextInt();
 
 
-//        Graph adjacency matrix with 0 and 1 entries for non-edge and edges respectively
+//        Graph adjacency matrix with 0 and 1 entries for non-edge and edges between nodes respectively
         int[][] graph = new int[nodes][nodes];
         System.out.println("Enter the adjacency matrix of the graph : ");
         for (int i = 0; i < nodes; i++) {
@@ -23,7 +23,7 @@ public class ZigZagProduct {
         System.out.println();
 
 
-//        Cycle graph to generate the cloud graphs with number of nodes equal to the degree of the actual graph
+//        Cycle graph to generate the cloud graphs with number of nodes equal to the degree of the original graph (cloud can provide manually)
         int[][] cloud = new int[degree][degree];
         for (int i = 0; i < degree; i++) {
             cloud[i][(i + 1) % degree] = 1;
@@ -69,22 +69,51 @@ public class ZigZagProduct {
             }
         }
 
-//        Cloud internal connection to form the complete graph with cloud instead of nodes in actual graph.
-        for (int i = 0; i < nodes; i++) {
+
+//        Edges connection between the nodes after placing the cloud in the original graph with the help of the cloudMap matrix
+        int[][] zigZagProduct = new int[dimension][dimension];
+
+        for (int i = 0; i < dimension; i++) {
+            int primaryCloud = i / degree;
+            int primaryCloudNode = i % 3;
             for (int j = 0; j < degree; j++) {
-                for (int k = 0; k < degree; k++) {
-                    cloudMap[i*degree + j][i*degree + k] = cloud[j][k];
+                if (cloud[primaryCloudNode][j] == 1) {
+                    for (int k = 0; k < dimension; k++) {
+                        if (cloudMap[primaryCloud * degree + j][k] == 1) {
+                            int secondaryCloud = k / degree;
+                            int secondaryCloudNode = k % degree;
+
+                            for (int l = 0; l < degree; l++) {
+                                if (cloud[k % degree][l] == 1) {
+                                    zigZagProduct[i][secondaryCloud * degree + l] = 1;
+                                    zigZagProduct[secondaryCloud * degree + l][i] = 1;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
 
+
+//        This is the output of the cloudMap after replacing the cloud in the original graph
+        System.out.println("Cloud Mapping of the given graph: ");
         for (int i = 0; i < dimension; i++) {
             for (int j = 0; j < dimension; j++) {
                 System.out.print(cloudMap[i][j] + " ");
             }
             System.out.println();
         }
+        System.out.println();
 
 
+//        This is the output of the actual zigZag product of two graphs
+        System.out.println("ZigZag product of the cloud and given graph: ");
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                System.out.print(zigZagProduct[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
